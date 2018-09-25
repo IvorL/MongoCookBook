42c6b9e27494111527985b5a934994c40548a954
Ivor Lishman <tlishman@spartaglobal.com>
2018-09-25 11:19:38 +0100

    mogod setup

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,44 @@ describe 'mongo::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'updates all sources' do
+      expect(chef_run).to update_apt_update('update')
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should add mongo to the sources list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
+    end
+
+    it 'should install mongodb' do
+      expect(chef_run).to upgrade_package('mongodb-org')
+    end
+
+    it 'should delete mongod.conf' do
+      expect(chef_run).to delete_file("/etc/mongod.conf")
+    end
+
+    it "should create a mongod.conf template on /etc/mongod.conf" do
+      expect(chef_run).to create_template("/etc/mongod.conf")
+    end
+
+    it "should create a mongod.service template on /lib/systemd/system/mongod.service" do
+      expect(chef_run).to create_template("/lib/systemd/system/mongod.service")
+    end
+
+    it "should check if mongod is enabled" do
+      expect(chef_run).to enable_service("mongod")
+    end
+
+    it "should check if mongod is started" do
+      expect(chef_run).to start_service("mongod")
     end
   end
 end
